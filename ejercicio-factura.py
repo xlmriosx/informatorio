e183cc82193550287d577fd37b604077bb9fb8ed
LeonardoReichert <leoreichert5000@gmail.com>
2023-09-23 12:37:39 -0300

    terminamos el ejercicio de promedio de factura

---
+++ ejercicio-factura.py
@@ -0,0 +1,22 @@
+
+
+#crear una coleccion de facturas y sacar el promedio
+# pedir al usuario primero la cantidad de facturas
+# usar una lista para este ejercicio
+
+
+facturas = []
+
+numero_de_facturas = int(input("ingrese cantidad de facturas:"))
+
+for num in range(1, numero_de_facturas+1):
+    monto = int(input(f"ingrese monto de factura {num}: "))
+    facturas.append(monto)
+
+suma = sum(facturas)
+
+promedio = suma / numero_de_facturas
+
+print("El promedio de las facturas es: ",promedio)
+
+
